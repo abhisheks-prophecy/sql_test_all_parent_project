dff878d1e09a91a27edf34d959fb79288ba3eac5
abhisheks@prophecy.io <abhisheks@prophecy.io>
2023-06-08 06:32:23 +0000

    ❌ Deleted     - Source        - spark_catalog.qa_database_acl        - spark_catalog.qa_database
    🔁 Modified
        - Model
           - test_sql_databricks_model_1
           - test_sql_databricks_useall

--- models/test_sql_databricks_useall.sql
+++ models/test_sql_databricks_useall.sql
@@ -4,62 +4,56 @@
 {% set v_model_string = 'hello' %}
 
 
-WITH table_with_read_permission_for_user AS (
+WITH goods_classification_test AS (
 
   SELECT * 
   
-  FROM {{ source('spark_catalog.qa_database_acl', 'table_with_read_permission_for_user') }}
+  FROM {{ ref('goods_classification_test')}}
 
 ),
 
-all_type_non_partitioned AS (
+service_classification_test AS (
 
   SELECT * 
   
-  FROM {{ source('spark_catalog.qa_database', 'all_type_non_partitioned') }}
+  FROM {{ ref('service_classification_test')}}
 
 ),
 
 Join_1 AS (
 
   SELECT 
-    in0.c_tinyint AS c_tinyint,
-    in0.c_smallint AS c_smallint,
-    in0.c_int AS c_int,
-    in0.c_bigint AS c_bigint,
-    in0.c_float AS c_float,
-    in0.c_double AS c_double,
-    in0.c_string AS c_string,
-    in0.c_boolean AS c_boolean,
-    in0.c_array AS c_array,
-    in0.c_struct AS c_struct
+    in0.code AS code,
+    in0.service_label AS service_label,
+    in1.NZHSC_Level_2_Code_HS4 AS NZHSC_Level_2_Code_HS4,
+    in1.NZHSC_Level_1_Code_HS2 AS NZHSC_Level_1_Code_HS2,
+    in1.NZHSC_Level_2 AS NZHSC_Level_2,
+    in1.NZHSC_Level_1 AS NZHSC_Level_1,
+    in1.Status_HS4 AS Status_HS4
   
-  FROM all_type_non_partitioned AS in0
-  INNER JOIN table_with_read_permission_for_user AS in1
-     ON in0.c_int != in1.a
+  FROM service_classification_test AS in0
+  INNER JOIN goods_classification_test AS in1
+     ON in0.code != in1.NZHSC_Level_1
 
 ),
 
 Reformat_1 AS (
 
   SELECT 
-    c_tinyint AS c_tinyint,
-    c_smallint AS c_smallint,
-    c_int AS c_int,
-    c_bigint AS c_bigint,
-    c_float AS c_float,
-    c_double AS c_double,
-    c_string AS c_string,
-    c_boolean AS c_boolean,
-    c_array AS c_array,
-    c_struct AS c_struct,
     {% if v_model_int > 10 %}
-      concat(c_string, c_int) AS c_if
+      concat(code, NZHSC_Level_1) AS c_if,
     {% elif  var('v_project_int') > 10 and v_model_string == "hello" %}
-      concat(c_string, c_bigint) AS c_if
+      concat(code, NZHSC_Level_1) AS c_if,
     {% else %}
-      concat(c_string, c_smallint) AS c_if
+      concat(code, NZHSC_Level_2) AS c_if,
     {% endif %}
+    code AS code,
+    service_label AS service_label,
+    NZHSC_Level_2_Code_HS4 AS NZHSC_Level_2_Code_HS4,
+    NZHSC_Level_1_Code_HS2 AS NZHSC_Level_1_Code_HS2,
+    NZHSC_Level_2 AS NZHSC_Level_2,
+    NZHSC_Level_1 AS NZHSC_Level_1,
+    Status_HS4 AS Status_HS4
   
   FROM Join_1 AS in0
 
